d17c59be1862f2a81b3a9912a3cfe62c422ba7a8
Guy Sartorelli <guy.sartorelli@gmail.com>
2024-11-11 11:51:39 +1300

    API Update code to reflect changes in dnadesign/silverstripe-elemental

--- app/src/BlocksPage.php
+++ app/src/BlocksPage.php
@@ -4,11 +4,6 @@ class BlocksPage extends Page
 {
     private static $table_name = 'BlocksPage';
 
-    /**
-     * @deprecated 5.4.0 use class_description instead.
-     */
-    private static $description = 'A modular page composed of content blocks';
-
     private static $class_description = 'A modular page composed of content blocks';
 
     private static $icon_class = 'font-icon-p-alt';
